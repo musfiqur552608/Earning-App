630ceb5d72d20bfb1b31ca3fc9352f495e1446fb
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-09-11 01:51:04 +0600

    Recyclerview add in home

--- app/src/main/java/com/example/earningapp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/earningapp/Fragment/HomeFragment.kt
@@ -5,56 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.earningapp.R
+import com.example.earningapp.adapter.CategoryAdapter
+import com.example.earningapp.databinding.FragmentHomeBinding
+import com.example.earningapp.model.categoryModelClass
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
     }
+    private var categoryList = ArrayList<categoryModelClass>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryList.add(categoryModelClass(R.drawable.c, "Learn C"))
+        categoryList.add(categoryModelClass(R.drawable.cplus, "Learn C++"))
+        categoryList.add(categoryModelClass(R.drawable.java, "Learn JAVA"))
+        categoryList.add(categoryModelClass(R.drawable.python, "Learn Python"))
+        binding.catRecyclerview.layoutManager= GridLayoutManager(requireContext(),2)
+
+        var adapter = CategoryAdapter(categoryList)
+        binding.catRecyclerview.adapter = adapter
+        binding.catRecyclerview.setHasFixedSize(true)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
--- app/src/main/java/com/example/earningapp/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/earningapp/adapter/CategoryAdapter.kt
@@ -7,19 +7,14 @@ import com.example.earningapp.databinding.CategoryitemBinding
 import com.example.earningapp.model.categoryModelClass
 
 class CategoryAdapter(var categoryList: ArrayList<categoryModelClass>):RecyclerView.Adapter<CategoryAdapter.MyCategoryViewHolder>() {
-    class MyCategoryViewHolder(var binding:CategoryitemBinding) : RecyclerView.ViewHolder(binding.root) {
-
-
-
-    }
+    class MyCategoryViewHolder(var binding:CategoryitemBinding) : RecyclerView.ViewHolder(binding.root) {}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyCategoryViewHolder {
         return MyCategoryViewHolder(CategoryitemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    override fun getItemCount(): Int {
-        return categoryList.size
-    }
+    override fun getItemCount() = categoryList.size
+
 
     override fun onBindViewHolder(holder: MyCategoryViewHolder, position: Int) {
         var datalist = categoryList[position]
