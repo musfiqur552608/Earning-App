5372c429ade6c5a3a7704d7df6d87f96745bbd58
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-09-12 23:24:37 +0600

    spinner done

--- app/src/main/java/com/example/earningapp/Fragment/SpinFragment.kt
+++ app/src/main/java/com/example/earningapp/Fragment/SpinFragment.kt
@@ -1,32 +1,25 @@
 package com.example.earningapp.Fragment
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.earningapp.R
+import android.widget.Toast
+import com.example.earningapp.databinding.FragmentSpinBinding
+import java.util.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SpinFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SpinFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSpinBinding
+    private lateinit var timer: CountDownTimer
+    private val itemTitles = arrayOf("100", "Try Again", "500", "Try Again", "200", "Try Again")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -34,27 +27,41 @@ class SpinFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spin, container, false)
+        binding = FragmentSpinBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    private fun showResult(itemTitle:String){
+        Toast.makeText(requireContext(), itemTitle, Toast.LENGTH_SHORT).show()
+        binding.spinBtn.isEnabled = true
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.spinBtn.setOnClickListener{
+            binding.spinBtn.isEnabled = false
+
+            val spin = Random().nextInt(6)
+            val degrees = 60f * spin
+
+            timer = object : CountDownTimer(5000, 50){
+                var rotation = 0f
+                override fun onTick(millisUntilFinished: Long){
+                    rotation += 5f
+                    if(rotation>=degrees){
+                        rotation = degrees
+                        timer.cancel()
+                        showResult(itemTitles[spin])
+                    }
+                    binding.spinWheel.rotation = rotation
+                }
+
+                override fun onFinish() {}
+            }.start()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpinFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
