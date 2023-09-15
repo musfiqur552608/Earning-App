6cc7c95795da394fb3070deae2ca34c1d47d3765
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-09-15 19:45:26 +0600

    Data save and retrive

--- app/src/main/java/com/example/earningapp/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/earningapp/Fragment/ProfileFragment.kt
@@ -7,6 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.earningapp.R
 import com.example.earningapp.databinding.FragmentProfileBinding
+import com.example.earningapp.model.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 
 class ProfileFragment : Fragment() {
@@ -36,6 +44,26 @@ class ProfileFragment : Fragment() {
             }
             isExpend = !isExpend
         }
+        Firebase.database.reference.child("Users")
+            .child(Firebase.auth.currentUser!!.uid)
+            .addListenerForSingleValueEvent(
+                object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        var user = snapshot.getValue<User>()
+
+                        binding.nameUser.text = user?.name
+                        binding.nametxt.text = user?.name
+                        binding.ageTxt.text = user?.age.toString()
+                        binding.emailTxt.text = user?.email
+                        binding.passTxt.text = user?.password
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+            )
         return binding.root
     }
 
--- app/src/main/java/com/example/earningapp/MainActivity.kt
+++ app/src/main/java/com/example/earningapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.earningapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.earningapp.databinding.ActivityMainBinding
+import com.example.earningapp.model.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.signupBtn.setOnClickListener {
+            if( binding.nameEtxt.text.toString().equals("")||
+                binding.ageEtxt.toString().equals("")||
+                binding.emailEtxt.text.toString().equals("")||
+                binding.passEtxt.text.toString().equals("")){
+                Toast.makeText(this, "Please fill all the details", Toast.LENGTH_SHORT).show()
+            }else{
+                Firebase.auth.createUserWithEmailAndPassword(binding.emailEtxt.text.toString(), binding.passEtxt.text.toString()).
+                addOnCompleteListener{
+                    if(it.isSuccessful){
+                        var user = User(binding.nameEtxt.text.toString(),
+                            binding.ageEtxt.text.toString().toInt(),
+                            binding.emailEtxt.text.toString(),
+                            binding.passEtxt.text.toString())
+                        Firebase.database.reference.child("Users")
+                            .child(Firebase.auth.currentUser!!.uid).setValue(user)
+                            .addOnSuccessListener {
+                                startActivity(Intent(this, HomeActivity::class.java))
+                                finish()
+                            }
+
+                    }else{
+                        Toast.makeText(this, it.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(Firebase.auth.currentUser!=null){
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
     }
 }
